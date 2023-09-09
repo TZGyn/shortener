58bb2a42eb369e6d61d664d9d5daff57b965534c
TZGyn <ca538901d@mozmail.com>
2023-09-10 00:10:34 +0800

    Added shadcn ui to frontend

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { ModeToggle } from './components/mode-toggle'
+import { Button } from './components/ui/button'
+import { ThemeProvider } from '@/components/theme-provider'
 
 function App() {
-  const [count, setCount] = useState(0)
+	return (
+		<ThemeProvider
+			defaultTheme='dark'
+			storageKey='vite-ui-theme'>
+			<Button>Shadcn</Button>
+			<AlertDialogDemo />
+			<ModeToggle />
+		</ThemeProvider>
+	)
+}
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+
+export function AlertDialogDemo() {
+	return (
+		<AlertDialog>
+			<AlertDialogTrigger asChild>
+				<Button variant='outline'>Show Dialog</Button>
+			</AlertDialogTrigger>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>
+						Are you absolutely sure?
+					</AlertDialogTitle>
+					<AlertDialogDescription>
+						This action cannot be undone. This will permanently
+						delete your account and remove your data from our
+						servers.
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+				<AlertDialogFooter>
+					<AlertDialogCancel>Cancel</AlertDialogCancel>
+					<AlertDialogAction>Continue</AlertDialogAction>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	)
 }
 
 export default App
