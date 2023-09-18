b56dc5efe26b9cd5b055974cbf6e8148f29e69d7
TZGyn <ca538901d@mozmail.com>
2023-09-19 06:06:24 +0800

    Frontend added react router

--- react-frontend/src/main.tsx
+++ react-frontend/src/main.tsx
@@ -2,9 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path='/'>
+					<Route
+						index
+						element={<App />}></Route>
+					<Route path='dashboard'>
+						<Route
+							index
+							element={<Navigate to='/' />}
+						/>
+					</Route>
+					<Route
+						path='*'
+						element={<Navigate to='/' />}
+					/>
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>
 )
