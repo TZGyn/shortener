7054052602d83d8ba97b5f3485aeddb2db124b23
TZGyn <ca538901d@mozmail.com>
2023-11-07 23:06:53 +0800

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.postcss';
+	import { ModeWatcher } from 'mode-watcher';
+	import ThemeToggle from '$lib/components/theme-toggle.svelte';
+</script>
+
+<ModeWatcher />
+<ThemeToggle />
+<slot />
