4d6e5a29f60cc0e2012b473bfdbd6c606f8e3875
TZGyn <ca538901d@mozmail.com>
2023-11-09 19:50:54 +0800

    update login page

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,34 +1,14 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button';
+	import ThemeToggle from '$lib/components/theme-toggle.svelte';
 	import UserAuthForm from './(components)/user-auth-form.svelte';
 </script>
 
-<div class="md:hidden">
-	<img
-		src="/examples/authentication-light.png"
-		width={1280}
-		height={843}
-		alt="Authentication"
-		class="block dark:hidden"
-	/>
-	<img
-		src="/examples/authentication-dark.png"
-		width={1280}
-		height={843}
-		alt="Authentication"
-		class="hidden dark:block"
-	/>
-</div>
 <div
-	class="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0"
+	class="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0"
 >
-	<Button
-		href="/examples/authentication"
-		variant="ghost"
-		class="absolute right-4 top-4 md:right-8 md:top-8"
-	>
-		Login
-	</Button>
+	<div class="absolute right-4 top-4 md:right-8 md:top-8">
+		<ThemeToggle />
+	</div>
 	<div class="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
 		<div
 			class="absolute inset-0 bg-cover"
@@ -36,21 +16,9 @@
 				background-image:
 					url(https://images.unsplash.com/photo-1590069261209-f8e9b8642343?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80);"
 		/>
-		<div class="relative z-20 flex items-center text-lg font-medium">
-			<!-- <Command class="mr-2 h-6 w-6" /> -->
-			Acme Inc
-		</div>
-		<div class="relative z-20 mt-auto">
-			<blockquote class="space-y-2">
-				<p class="text-lg">
-					&ldquo;This library has saved me countless hours of work and helped me deliver stunning
-					designs to my clients faster than ever before. Highly recommended!&rdquo;
-				</p>
-				<footer class="text-sm">Sofia Davis</footer>
-			</blockquote>
-		</div>
+		<div class="relative z-20 flex items-center text-lg font-medium">Shortener</div>
 	</div>
-	<div class="lg:p-8">
+	<div class="p-8">
 		<div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
 			<div class="flex flex-col space-y-2 text-center">
 				<h1 class="text-2xl font-semibold tracking-tight">Create an account</h1>
