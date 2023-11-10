c1f1c81ff0950c24a1f9f273dd3fb5a442aec1a2
TZGyn <ca538901d@mozmail.com>
2023-11-11 00:03:22 +0800

    removed unused code + update env

--- src/index.ts
+++ src/index.ts
@@ -13,44 +13,6 @@ const app = new Elysia().use(cors())
 app.get('/', () => 'Hello Elysia')
 app.get('/invalid', () => 'Invalid Shortener')
 
-app.get('/link', async () => {
-	const shorteners = await db
-		.selectFrom('shortener')
-		.leftJoin('visitor', 'visitor.shortener_id', 'shortener.id')
-		.select(({ fn }) => [
-			'shortener.id',
-			'shortener.link',
-			'shortener.code',
-			'shortener.created_at',
-			fn.count<number>('visitor.id').as('visitor_count'),
-		])
-		.groupBy('shortener.id')
-		.execute()
-
-	return { shorteners }
-})
-
-app.post('/link', async ({ body, set }) => {
-	const createLink = createLinkSchema.safeParse(body)
-
-	if (!createLink.success) {
-		set.status = 400
-		return { message: 'Invalid Link', body }
-	}
-
-	const uuid = nanoid(10)
-
-	await db
-		.insertInto('shortener')
-		.values({
-			link: createLink.data.link,
-			code: uuid,
-		})
-		.execute()
-
-	return { message: 'Success' }
-})
-
 app.get(
 	'/:shortenerCode',
 	async ({ params: { shortenerCode }, set, request }) => {
@@ -89,99 +51,11 @@ app.get(
 	}
 )
 
-app.get('/link/:shortenerCode', async ({ params: { shortenerCode } }) => {
-	try {
-		const shorteners = await db
-			.selectFrom('shortener')
-			.select((shortener) => [
-				'id',
-				'code',
-				'link',
-				'created_at',
-				jsonArrayFrom(
-					shortener
-						.selectFrom('visitor')
-						.select([
-							'visitor.created_at as visited_at',
-							'visitor.country_code',
-						])
-						.whereRef('visitor.shortener_id', '=', 'shortener.id')
-				).as('visitors'),
-			])
-			.where('code', '=', shortenerCode)
-			.execute()
-
-		const visitors = await db
-			.selectFrom('visitor')
-			.select(({ fn }) => [
-				'visitor.country_code',
-				'visitor.country',
-				fn.count<number>('visitor.id').as('visitor_count'),
-			])
-			.where('visitor.shortener_id', '=', shorteners[0].id)
-			.groupBy(['visitor.country_code', 'visitor.country'])
-			.execute()
-
-		return { shorteners, visitors }
-	} catch {
-		return { error: true }
-	}
-})
-
-app.post(
-	'/signup',
-	async ({ body, set }) => {
-		const { email, username, password, password_confirm } = body
-
-		const { error } = await signup(
-			email,
-			username,
-			password,
-			password_confirm
-		)
-
-		if (error) {
-			set.status = 400
-			return { error }
-		}
-
-		return { message: 'User Successfully Created' }
-	},
-	{
-		body: t.Object({
-			username: t.String(),
-			email: t.String(),
-			password: t.String(),
-			password_confirm: t.String(),
-		}),
-	}
-)
-
-app.post(
-	'/login',
-	async ({ body, set }) => {
-		const { email, password } = body
-		const { user, error } = await login(email, password)
-
-		if (error) {
-			set.status = 400
-			return { error }
-		} else {
-			return user
-		}
-	},
-	{
-		body: t.Object({
-			email: t.String(),
-			password: t.String(),
-		}),
-	}
-)
-
 app.listen(3000)
 
 console.log(
-	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+	`${Bun.env.database}`
 )
 
 export type App = typeof app
