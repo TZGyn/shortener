b648cde8bcbd5268710b9af061a426aa3b8f13f5
TZGyn <ca538901d@mozmail.com>
2023-11-10 23:50:54 +0800

    init

---
+++ src/database.ts
@@ -0,0 +1,18 @@
+import { Database } from "./types";
+import { Pool } from "pg";
+import { Kysely, PostgresDialect } from "kysely";
+
+const dialect = new PostgresDialect({
+  pool: new Pool({
+    database: Bun.env.database ?? "link-shortener",
+    host: Bun.env.host ?? "0.0.0.0",
+    user: Bun.env.user ?? "postgres",
+    password: Bun.env.password ?? "password",
+    port: parseInt(Bun.env.port ?? "") ?? 5432,
+    max: 10,
+  }),
+});
+
+export const db = new Kysely<Database>({
+  dialect,
+});
