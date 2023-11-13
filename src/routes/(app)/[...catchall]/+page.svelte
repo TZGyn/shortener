1c44bfb2a8fa7bb06a3dbcf47c1dcd8feea7663d
TZGyn <ca538901d@mozmail.com>
2023-11-14 04:46:18 +0800

    moved catchall route to app layout

---
+++ src/routes/(app)/[...catchall]/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { goto } from '$app/navigation'
+	import Button from '$lib/components/ui/button/button.svelte'
+</script>
+
+<div class="w-full h-full flex justify-center items-center">
+	<div class="flex flex-col gap-12 items-center">
+		<div class="flex flex-col gap-4 items-center">
+			<div class="text-4xl font-bold">404</div>
+			<div class="text-4xl font-bold">Page Not Found</div>
+		</div>
+		<Button on:click={() => goto('/')} class="w-fit">Return Home</Button>
+	</div>
+</div>
