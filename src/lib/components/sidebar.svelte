cecce351eeef8b6500bc3ead09055d88dc224010
TZGyn <ca538901d@mozmail.com>
2023-11-14 04:46:01 +0800

    added avatar to sidebar

--- src/lib/components/sidebar.svelte
+++ src/lib/components/sidebar.svelte
@@ -1,10 +1,15 @@
-<script>
+<script lang="ts">
 	import { Separator } from '$lib/components/ui/separator'
 	import ThemeToggle from './theme-toggle.svelte'
 	import { Button } from '$lib/components/ui/button'
+	import * as Avatar from '$lib/components/ui/avatar'
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu'
+
+	import { User } from 'lucide-svelte'
+	import { goto } from '$app/navigation'
 </script>
 
-<div class="w-[400px] p-4 border-r h-full flex flex-col justify-between">
+<div class="min-w-[350px] p-4 border-r h-full flex flex-col justify-between">
 	<div>
 		<div class="flex justify-between pb-16 items-center">
 			<div class="text-xl font-bold">Shortener</div>
@@ -15,6 +20,23 @@
 		<Separator />
 	</div>
 	<div class="flex justify-between items-center">
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger>
+				<Avatar.Root>
+					<Avatar.Image src="github.com/shadcn.png" alt="@shadcn" />
+					<Avatar.Fallback><User /></Avatar.Fallback>
+				</Avatar.Root>
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content>
+				<DropdownMenu.Group>
+					<DropdownMenu.Label>My Account</DropdownMenu.Label>
+					<DropdownMenu.Separator />
+					<DropdownMenu.Item on:click={() => goto('/profile')}
+						>Profile</DropdownMenu.Item
+					>
+				</DropdownMenu.Group>
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
 		<form method="post" action="?/signout">
 			<Button variant="destructive" type="submit">Sign Out</Button>
 		</form>
