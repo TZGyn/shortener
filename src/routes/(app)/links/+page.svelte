6205687d5020603212d200008b9049addddb4ba6
TZGyn <ca538901d@mozmail.com>
2023-11-15 00:45:06 +0800

    added links route

---
+++ src/routes/(app)/links/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { PageData } from './$types'
+	import { Separator } from '$lib/components/ui/separator'
+
+	export let data: PageData
+</script>
+
+<div class="p-8 text-4xl font-bold">Links</div>
+<Separator />
