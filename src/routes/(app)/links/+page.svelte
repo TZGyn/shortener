1fb17df71e5d9ebfc5b1bec05a3229d7ca0a295f
TZGyn <ca538901d@mozmail.com>
2023-11-17 19:49:45 +0800

    added insert shortener api

--- src/routes/(app)/links/+page.svelte
+++ src/routes/(app)/links/+page.svelte
@@ -1,12 +1,66 @@
 <script lang="ts">
 	import type { PageData } from './$types'
 	import { Separator } from '$lib/components/ui/separator'
+	import { Button, buttonVariants } from '$lib/components/ui/button'
+	import * as Dialog from '$lib/components/ui/dialog'
+	import { Input } from '$lib/components/ui/input'
+	import { Label } from '$lib/components/ui/label'
+	import { Loader2, PlusCircle } from 'lucide-svelte'
+	import { invalidateAll } from '$app/navigation'
 
 	export let data: PageData
-	console.log('data', data)
+
+	let dialogOpen = false
+	let inputLink = ''
+	let isLoading = false
+	const addShortener = async () => {
+		isLoading = true
+
+		await fetch('/api/shortener', {
+			method: 'post',
+			body: JSON.stringify({ link: inputLink }),
+		})
+
+		isLoading = false
+		await invalidateAll()
+		dialogOpen = false
+	}
+	$: console.log(inputLink)
 </script>
 
-<div class="p-8 text-4xl font-bold">Links</div>
+<div class="flex justify-between p-8">
+	<div class="text-4xl font-bold">Links</div>
+	<Dialog.Root bind:open={dialogOpen}>
+		<Dialog.Trigger
+			class={buttonVariants({ variant: 'default' }) + 'flex gap-2'}
+		>
+			<PlusCircle />
+			Add Shortner
+		</Dialog.Trigger>
+		<Dialog.Content class="sm:max-w-[425px]">
+			<Dialog.Header>
+				<Dialog.Title>Add Shortener</Dialog.Title>
+				<Dialog.Description>
+					Create A New Shortner Here. Click Add To Save.
+				</Dialog.Description>
+			</Dialog.Header>
+			<div class="grid gap-4 py-4">
+				<div class="grid grid-cols-4 items-center gap-4">
+					<Label class="text-right">Link</Label>
+					<Input id="name" bind:value={inputLink} class="col-span-3" />
+				</div>
+			</div>
+			<Dialog.Footer>
+				<Button on:click={addShortener} class="flex gap-2">
+					{#if isLoading}
+						<Loader2 class="animate-spin" />
+					{/if}
+					Add</Button
+				>
+			</Dialog.Footer>
+		</Dialog.Content>
+	</Dialog.Root>
+</div>
 <Separator />
 
 {#if data.shorteners.length > 0}
