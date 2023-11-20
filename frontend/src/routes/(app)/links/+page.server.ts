19b9429abc2cc747f2f5e448bc6c55ace0cf528f
TZGyn <ca538901d@mozmail.com>
2023-11-21 06:41:11 +0800

    update link page.server.ts to use userObject locals

--- frontend/src/routes/(app)/links/+page.server.ts
+++ frontend/src/routes/(app)/links/+page.server.ts
@@ -1,19 +1,14 @@
 import { db } from '$lib/db'
 import type { PageServerLoad } from './$types'
-import { redirect } from '@sveltejs/kit'
 
 export const load = (async (event) => {
-	if (!event.locals.user) {
-		throw redirect(303, '/')
-	}
-
-	const userId = event.locals.user
+	const user = event.locals.userObject
 
 	const shorteners = await db.query.shortener.findMany({
 		with: {
 			visitor: true,
 		},
-		where: (shortener, { eq }) => eq(shortener.userId, userId),
+		where: (shortener, { eq }) => eq(shortener.userId, user.id),
 	})
 
 	return { shorteners }
