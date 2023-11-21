cbcad8198b6fb5f6a90b8212cbfe9d0852126180
TZGyn <ca538901d@mozmail.com>
2023-11-21 09:41:32 +0800

    redirect backend changed database env to use connection string

--- redirect/src/database.ts
+++ redirect/src/database.ts
@@ -4,11 +4,9 @@ import { Kysely, PostgresDialect } from 'kysely'
 
 const dialect = new PostgresDialect({
 	pool: new Pool({
-		database: Bun.env.PGDATABASE ?? 'link-shortener',
-		host: Bun.env.PGHOST ?? '0.0.0.0',
-		user: Bun.env.PGUSER ?? 'postgres',
-		password: Bun.env.PGPASSWORD ?? 'password',
-		port: parseInt(Bun.env.PGPORT ?? '') ?? 5432,
+		connectionString:
+			Bun.env.DATABASE_URL ??
+			'postgres://postgres:password@0.0.0.0:5432/link-shortener',
 		max: 10,
 	}),
 })
