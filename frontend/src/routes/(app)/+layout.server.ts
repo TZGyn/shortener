1f0828a7663af4806eba8cc507e72c20bf3bfb68
TZGyn <ca538901d@mozmail.com>
2023-11-21 13:41:59 +0800

    removed PUBLIC_SHORTENER_URL from sveltekit env

--- frontend/src/routes/(app)/+layout.server.ts
+++ frontend/src/routes/(app)/+layout.server.ts
@@ -1,5 +1,4 @@
 import type { LayoutServerLoad } from './$types'
-import { PUBLIC_SHORTENER_URL } from '$env/static/public'
 
 export const load = (async (event) => {
 	const user = event.locals.userObject
