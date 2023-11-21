b351f7fb332586a6075af7f517d4a17b8b024c97
TZGyn <ca538901d@mozmail.com>
2023-11-21 09:41:01 +0800

    added shortener url env

--- frontend/src/routes/(app)/+layout.server.ts
+++ frontend/src/routes/(app)/+layout.server.ts
@@ -1,10 +1,11 @@
 import type { LayoutServerLoad } from './$types'
+import { PUBLIC_SHORTENER_URL } from '$env/static/public'
 
 export const load = (async (event) => {
 	const user = event.locals.userObject
 
 	return {
-		shortener_url: process.env.SHORTENER_URL ?? 's.tzgyn.com',
+		shortener_url: PUBLIC_SHORTENER_URL,
 		user: user,
 	}
 }) satisfies LayoutServerLoad
