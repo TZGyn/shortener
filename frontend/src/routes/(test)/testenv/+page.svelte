86cb005ab1e79a9ceb776e1b550c9bca86f1cb9d
TZGyn <ca538901d@mozmail.com>
2024-05-03 02:49:44 +0800

    remove using static env

--- frontend/src/routes/(test)/testenv/+page.svelte
+++ frontend/src/routes/(test)/testenv/+page.svelte
@@ -1,7 +1,5 @@
 <script>
-	import { PUBLIC_SHORTENER_URL } from '$env/static/public'
 	import { env } from '$env/dynamic/public'
 </script>
 
-<div>{PUBLIC_SHORTENER_URL}</div>
 <div>Dynamic env: {env.PUBLIC_SHORTENER_URL}</div>
