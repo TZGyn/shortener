e24fc63754fb4b36da4eaa31ce413da6db33f1ea
TZGyn <ca538901d@mozmail.com>
2024-05-03 02:47:26 +0800

    update dockerfile to sync env

--- frontend/src/routes/(test)/testenv/+page.svelte
+++ frontend/src/routes/(test)/testenv/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import { PUBLIC_SHORTENER_URL } from '$env/static/public'
+	import { env } from '$env/dynamic/public'
 </script>
 
 <div>{PUBLIC_SHORTENER_URL}</div>
+<div>Dynamic env: {env.PUBLIC_SHORTENER_URL}</div>
