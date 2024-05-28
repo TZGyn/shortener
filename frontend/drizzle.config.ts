aa2907dad78cdc73afc822c572a17132a699f709
TZGyn <ca538901d@mozmail.com>
2024-05-29 00:06:19 +0800

    update drizzlekit and added db push script

--- frontend/drizzle.config.ts
+++ frontend/drizzle.config.ts
@@ -4,11 +4,11 @@ import type { Config } from 'drizzle-kit'
 export default {
 	schema: './src/lib/db/schema.ts',
 	out: './drizzle',
-	driver: 'pg',
+	dialect: 'postgresql',
 	dbCredentials: {
 		user: 'postgres',
 		password: 'password',
-		host: '0.0.0.0',
+		host: '192.168.100.110',
 		port: 5432,
 		database: 'link-shortener',
 	},
