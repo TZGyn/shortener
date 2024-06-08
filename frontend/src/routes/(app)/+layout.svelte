4bfecfc5d2ae03351d3855379eb56f42725168cb
TZGyn <ca538901d@mozmail.com>
2024-06-09 05:46:13 +0800

    moved navbar drawer trigger to topbar

--- frontend/src/routes/(app)/+layout.svelte
+++ frontend/src/routes/(app)/+layout.svelte
@@ -20,10 +20,58 @@
 </script>
 
 <div
-	class="flex overflow-hidden flex-col w-full h-screen max-h-screen max-w-screen">
-	<div class="flex border-b bg-muted/40">
+	class="max-w-screen flex h-screen max-h-screen w-full flex-col overflow-hidden">
+	<div class="bg-muted/40 flex border-b">
 		<div
-			class="flex gap-6 justify-between items-center py-2 px-4 w-full">
+			class="flex w-full items-center justify-between gap-2 px-4 py-2">
+			<Sheet.Root bind:open={sheetOpen}>
+				<Sheet.Trigger asChild let:builder>
+					<Button
+						builders={[builder]}
+						variant="ghost"
+						class="p-2 lg:hidden">
+						<Menu />
+					</Button>
+				</Sheet.Trigger>
+				<Sheet.Content side="left" class="flex flex-col">
+					<Sheet.Header class="pb-16">
+						<Sheet.Title>Shortener</Sheet.Title>
+					</Sheet.Header>
+					<div class="flex grow flex-col gap-4">
+						<Button
+							on:click={closeSheet}
+							variant="ghost"
+							href="/"
+							class="justify-start text-base">Home</Button>
+						<Button
+							on:click={closeSheet}
+							variant="ghost"
+							href="/links"
+							class="justify-start text-base">Links</Button>
+						<Button
+							on:click={closeSheet}
+							variant="ghost"
+							href="/projects"
+							class="justify-start text-base">Projects</Button>
+						<Button
+							on:click={closeSheet}
+							variant="ghost"
+							href="/settings"
+							class="justify-start text-base">Settings</Button>
+					</div>
+					<div class="flex flex-col justify-between">
+						<div></div>
+						<div class="flex flex-col gap-4">
+							<Separator />
+							<div class="flex items-center justify-between">
+								<UserIcon
+									email={data.user.email}
+									onClick={closeSheet} />
+							</div>
+						</div>
+					</div>
+				</Sheet.Content>
+			</Sheet.Root>
 			<UserIcon email={data.user.email} />
 
 			<div class="w-full">
@@ -60,58 +108,9 @@
 		</div>
 	</div>
 
-	<div
-		class="block py-2 px-4 w-full border-b lg:hidden bg-background">
-		<Sheet.Root bind:open={sheetOpen}>
-			<Sheet.Trigger asChild let:builder>
-				<Button builders={[builder]} variant="ghost" class="p-2">
-					<Menu />
-				</Button>
-			</Sheet.Trigger>
-			<Sheet.Content side="left" class="flex flex-col">
-				<Sheet.Header class="pb-16">
-					<Sheet.Title>Shortener</Sheet.Title>
-				</Sheet.Header>
-				<div class="flex flex-col gap-4 grow">
-					<Button
-						on:click={closeSheet}
-						variant="ghost"
-						href="/"
-						class="justify-start text-base">Home</Button>
-					<Button
-						on:click={closeSheet}
-						variant="ghost"
-						href="/links"
-						class="justify-start text-base">Links</Button>
-					<Button
-						on:click={closeSheet}
-						variant="ghost"
-						href="/projects"
-						class="justify-start text-base">Projects</Button>
-					<Button
-						on:click={closeSheet}
-						variant="ghost"
-						href="/settings"
-						class="justify-start text-base">Settings</Button>
-				</div>
-				<div class="flex flex-col justify-between">
-					<div></div>
-					<div class="flex flex-col gap-4">
-						<Separator />
-						<div class="flex justify-between items-center">
-							<UserIcon
-								email={data.user.email}
-								onClick={closeSheet} />
-						</div>
-					</div>
-				</div>
-			</Sheet.Content>
-		</Sheet.Root>
-	</div>
-
-	<div class="flex overflow-hidden flex-grow">
+	<div class="flex flex-grow overflow-hidden">
 		<Sidebar class="hidden lg:flex" />
-		<div class="flex flex-col w-full h-auto">
+		<div class="flex h-auto w-full flex-col">
 			<slot />
 		</div>
 	</div>
