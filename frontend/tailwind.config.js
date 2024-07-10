0f7a1a298b94827c3a8008b32aef8b1d3ea76ca8
TZGyn <ca538901d@mozmail.com>
2024-07-11 07:57:09 +0800

    update tsconfig + added new color variables to tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -31,9 +31,19 @@ const config = {
 						'hsl(var(--secondary-foreground) / <alpha-value>)',
 				},
 				destructive: {
-					DEFAULT: 'var(--destructive)',
+					DEFAULT: 'hsl(var(--destructive) / <alpha-value>)',
 					foreground:
 						'hsl(var(--destructive-foreground) / <alpha-value>)',
+				},
+				success: {
+					DEFAULT: 'hsl(var(--success) / <alpha-value>)',
+					foreground:
+						'hsl(var(--success-foreground) / <alpha-value>)',
+				},
+				warning: {
+					DEFAULT: 'hsl(var(--warning) / <alpha-value>)',
+					foreground:
+						'hsl(var(--warning-foreground) / <alpha-value>)',
 				},
 				muted: {
 					DEFAULT: 'hsl(var(--muted) / <alpha-value>)',
