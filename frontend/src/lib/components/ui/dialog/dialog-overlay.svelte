63af56365291d0f4a57c9577d7087abfa054cf4e
TZGyn <ca538901d@mozmail.com>
2024-08-01 10:30:20 +0800

    update dialog component

--- frontend/src/lib/components/ui/dialog/dialog-overlay.svelte
+++ frontend/src/lib/components/ui/dialog/dialog-overlay.svelte
@@ -1,23 +1,21 @@
 <script lang="ts">
-	import { Dialog as DialogPrimitive } from 'bits-ui'
-	import { cn } from '$lib/utils'
-	import { fade } from 'svelte/transition'
+	import { Dialog as DialogPrimitive } from "bits-ui";
+	import { fade } from "svelte/transition";
+	import { cn } from "$lib/utils.js";
 
-	type $$Props = DialogPrimitive.OverlayProps
+	type $$Props = DialogPrimitive.OverlayProps;
 
-	let className: $$Props['class'] = undefined
-	export let transition: $$Props['transition'] = fade
-	export let transitionConfig: $$Props['transitionConfig'] = {
+	let className: $$Props["class"] = undefined;
+	export let transition: $$Props["transition"] = fade;
+	export let transitionConfig: $$Props["transitionConfig"] = {
 		duration: 150,
-	}
-	export { className as class }
+	};
+	export { className as class };
 </script>
 
 <DialogPrimitive.Overlay
 	{transition}
 	{transitionConfig}
-	class={cn(
-		'fixed inset-0 z-50 bg-background/80 backdrop-blur-sm',
-		className,
-	)}
-	{...$$restProps} />
+	class={cn("bg-background/80 fixed inset-0 z-50 backdrop-blur-sm", className)}
+	{...$$restProps}
+/>
