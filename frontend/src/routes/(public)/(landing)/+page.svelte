f9f3db6a1f46e1437199838752e2daca35539475
TZGyn <ca538901d@mozmail.com>
2024-09-23 07:47:29 +0800

    update pricing table layout

--- frontend/src/routes/(public)/(landing)/+page.svelte
+++ frontend/src/routes/(public)/(landing)/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button'
-	import { Separator } from '$lib/components/ui/separator'
 	import {
 		Check,
 		QrCodeIcon,
-		XIcon,
 		ChartAreaIcon,
 		ExternalLinkIcon,
 	} from 'lucide-svelte'
@@ -75,120 +73,116 @@
 			<div
 				class="flex w-full flex-col items-center gap-10 text-center">
 				<h2 class="text-4xl font-bold">Pricing</h2>
+				<p class="max-w-4xl text-center text-xl font-semibold">
+					Kon.sh only charges for domain & QR customization and longer
+					click history.
+				</p>
 				<div
-					class="grid w-full grid-cols-[repeat(auto-fit,_minmax(450px,_1fr))] gap-4">
-					<div class="flex gap-8 rounded-lg border p-8">
-						<div class="flex w-full flex-col gap-4 text-start">
-							<h3 class="text-2xl font-bold">Free</h3>
-							<p class="text-muted-foreground">
-								No credit card required.
+					class="grid w-full flex-col justify-center gap-4 sm:grid-cols-2 lg:grid-cols-3">
+					<div
+						class="relative flex flex-col gap-8 overflow-hidden rounded-2xl border p-4">
+						<div class="flex flex-col items-start">
+							<h3 class="text-xl font-semibold">Free</h3>
+							<p class="text-muted-foreground text-sm">
+								Essentials only. No credit card needed.
 							</p>
-							<Separator />
-							<div class="flex flex-col gap-4">
-								<div class="flex items-center gap-2">
-									<Check class="text-brand" />
-									<p>Unlimited Links</p>
-								</div>
-								<div class="flex items-center gap-2">
-									<Check class="text-brand" />
-									<p>Unlimited Projects</p>
-								</div>
-								<div class="flex items-center gap-2">
-									<Check class="text-brand" />
-									<p>Click history up to 1 month</p>
-								</div>
-							</div>
 						</div>
-						<div
-							class="flex w-full flex-col items-center justify-center gap-4 rounded-xl bg-gradient-to-r from-orange-300 to-orange-400 text-start">
-							<div class="flex items-end gap-2">
-								<p class="text-3xl font-bold">$0</p>
-								<span>/month</span>
-							</div>
-							<Button href="/signup" class="bg-background">
-								Get Started
-							</Button>
+						<div class="flex flex-row items-end gap-2">
+							<span class="text-4xl font-bold">$0</span>
+							<span>/ month</span>
 						</div>
+						<Button
+							href="/signup"
+							class="bg-brand hover:bg-brand-hover">
+							Get Started
+						</Button>
+
+						<ul class="flex flex-col gap-4 font-normal">
+							<li class="flex items-center gap-3">
+								<Check class="text-brand" />
+								<span class="flex">Unlimited Links</span>
+							</li>
+							<li class="flex items-center gap-3">
+								<Check class="text-brand" />
+								<span class="flex">Unlimited Projects</span>
+							</li>
+							<li class="flex items-center gap-3">
+								<Check class="text-brand" />
+								<span class="flex">Click history up to 1 month</span>
+							</li>
+						</ul>
 					</div>
-					<div class="flex gap-8 rounded-lg border p-8">
-						<div class="flex w-full flex-col gap-4 text-start">
-							<h3 class="text-2xl font-bold">Pro</h3>
-							<p class="text-muted-foreground">
+					<div
+						class="border-brand relative flex flex-col gap-8 overflow-hidden rounded-2xl border-2 p-4">
+						<div class="flex flex-col items-start">
+							<h3 class="text-xl font-semibold">Pro</h3>
+							<p class="text-muted-foreground text-sm">
 								More customizations.
 							</p>
-							<Separator />
-							<div class="flex flex-col gap-4">
-								<div class="flex items-center gap-2">
-									<Check class="text-brand" />
-									<p>Everything in free</p>
-								</div>
-								<div class="flex items-center gap-2">
-									<Check class="text-brand" />
-									<p>5 custom domains</p>
-									<!-- <Tooltip.Root>
-								<Tooltip.Trigger class="flex items-center gap-1">
-									<InfoIcon class="h-4 w-4" />
-								</Tooltip.Trigger>
-								<Tooltip.Content>
-									Additional domains are $3 per domain
-								</Tooltip.Content>
-							</Tooltip.Root> -->
-								</div>
-								<div class="flex items-center gap-2">
-									<Check class="text-brand" />
-									<p>2 years click history</p>
-								</div>
-								<div class="flex items-center gap-2">
-									<Check class="text-brand" />
-									<p>Custom QR Code</p>
-								</div>
-							</div>
 						</div>
-						<div
-							class="flex w-full flex-col items-center justify-center gap-4 rounded-xl bg-gradient-to-r from-orange-300 to-orange-400 text-start">
-							<div class="flex items-end gap-2">
-								<p class="text-3xl font-bold">$9</p>
-								<span>/month</span>
-							</div>
-							<Button
-								href={`/signup?redirect=${encodeURIComponent(
-									'/dashboard/billing',
-								)}`}
-								class="bg-background">
-								Get Started
-							</Button>
+						<div class="flex flex-row items-end gap-2">
+							<span class="text-4xl font-bold">$9</span>
+							<span>/ month</span>
 						</div>
+						<Button
+							href={`/signup?redirect=${encodeURIComponent(
+								'/dashboard/billing',
+							)}`}
+							class="bg-brand hover:bg-brand-hover">
+							Get Started
+						</Button>
+
+						<ul class="flex flex-col gap-4 font-normal">
+							<li class="flex items-center gap-3">
+								<Check class="text-brand" />
+								<span class="flex">Everything in free</span>
+							</li>
+							<li class="flex items-center gap-3">
+								<Check class="text-brand" />
+								<span class="flex">5 custom domains</span>
+							</li>
+							<li class="flex items-center gap-3">
+								<Check class="text-brand" />
+								<span class="flex">2 years click history</span>
+							</li>
+							<li class="flex items-center gap-3">
+								<Check class="text-brand" />
+								<span class="flex">Custom QR Code</span>
+							</li>
+						</ul>
 					</div>
-					<div class="flex gap-8 rounded-lg border p-8">
-						<div class="flex w-full flex-col gap-4 text-start">
-							<h3 class="text-2xl font-bold">Free (Self Host)</h3>
-							<p>No credit card required.</p>
-							<Separator />
-							<div class="flex flex-col gap-4">
-								<div class="flex items-center gap-2">
-									<Check class="text-brand" />
-									<p>Unlimited Links</p>
-								</div>
-								<div class="flex items-center gap-2">
-									<Check class="text-brand" />
-									<p>Unlimited Projects</p>
-								</div>
-								<div class="flex items-center gap-2">
-									<Check class="text-brand" />
-									<p>Basically No Limits</p>
-								</div>
-							</div>
+					<div
+						class="relative flex flex-col gap-8 overflow-hidden rounded-2xl border p-4">
+						<div class="flex flex-col items-start">
+							<h3 class="text-xl font-semibold">Free (Self Host)</h3>
+							<p class="text-muted-foreground text-sm">
+								You host on your server.
+							</p>
 						</div>
-						<div
-							class="flex w-full flex-col items-center justify-center gap-4 rounded-xl bg-gradient-to-r from-orange-300 to-orange-400 text-start">
-							<div class="flex items-end gap-2">
-								<p class="text-3xl font-bold">$0</p>
-								<span>/month</span>
-							</div>
-							<Button href="/signup" class="bg-background">
-								Get Started
-							</Button>
+						<div class="flex flex-row items-end gap-2">
+							<span class="text-4xl font-bold">$0</span>
+							<span>/ month</span>
 						</div>
+						<Button
+							href="/signup"
+							class="bg-brand hover:bg-brand-hover">
+							Get Started
+						</Button>
+
+						<ul class="flex flex-col gap-4 font-normal">
+							<li class="flex items-center gap-3">
+								<Check class="text-brand" />
+								<span class="flex">Unlimited Links</span>
+							</li>
+							<li class="flex items-center gap-3">
+								<Check class="text-brand" />
+								<span class="flex">Unlimited Projects</span>
+							</li>
+							<li class="flex items-center gap-3">
+								<Check class="text-brand" />
+								<span class="flex">Basically No Limits</span>
+							</li>
+						</ul>
 					</div>
 				</div>
 			</div>
